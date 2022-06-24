8acad7318ab78cf133d90a4a1932b0d7e895d838
Joseph Genchik <jgenchik@gmail.com>
2022-06-24 13:53:36 -0400

    [router] everything but no styling

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-first',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
-  constructor() { }
+  ctr = 0;
+  ctr2 = 0;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.ctr = this.route.snapshot.queryParams['ctr'];
+
+    this.route.queryParamMap.pipe(
+      tap(() => console.log('changed')),
+      tap(queryParams => {
+        const ctr = queryParams.get('ctr');
+        this.ctr2 = Number(queryParams.get('ctr'));
+      })
+    ).subscribe();
+
+  }
+
+  addToCounter() {
+
+    this.ctr++;
+
+    const newCtr = ++this.ctr2;
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParamsHandling: 'merge',
+      queryParams: { ctr: newCtr }
+    });
+
   }
 
 }
